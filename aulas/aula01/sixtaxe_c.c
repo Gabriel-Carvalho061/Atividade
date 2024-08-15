c1c1d8cef5beaafad7b66af423fb8b246b82a397
Gabriel-Carvalho061 <bielmengo07@gmail.com>
2024-08-15 00:49:30 +0000

    codigos da aula01

---
+++ aulas/aula01/sixtaxe_c.c
@@ -0,0 +1,41 @@
+//ISSO eh diferente de isso
+
+//aqui eh para calcular a area do retangulo
+
+/*area_retangulo = base * altura;*/
+
+// main() {
+// linha_de_codigo();
+// }
+
+
+// blocos { }
+#include <stdio.h>  // inclusao de bibliotecas
+
+  int main() {  
+  
+  // função principal
+  // seu codigo aqui
+
+  // 1. entrada
+  // ingredientes
+  // char, int, float,
+  // variaveis
+  // leitura 
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 2;
+  segundo_numero = 16;
+  
+  // 2. processamento
+  // calculo soma, subtração, multiplica, divisao
+  int multiplicacao = primeiro_numero * segundo_numero;
+
+
+  // 3. saida
+  // escrita
+  printf("a multiplicacao de %i com %i eh %i\n", primeiro_numero, segundo_numero, multiplicacao);
+  return 0; 
+  
+  // return fim do programa
+}
---
+++ praticas/praticas01/oi_mundo.c
@@ -0,0 +1,24 @@
+ int main() {  
+
+  // função principal
+  // seu codigo aqui
+
+  // 1. entrada
+  // ingredientes
+  // char, int, float,
+  // variaveis
+  // leitura 
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 14;
+  segundo_numero = 68;
+
+  // 2. processamento
+  // calculo soma, subtração, multiplica, divisao
+  int soma = primeiro_numero + segundo_numero;
+
+
+  // 3. saida
+  // escrita
+  printf("a soma de %i com %i eh %i\n", primeiro_numero, segundo_numero, soma);
+  return 0; 
